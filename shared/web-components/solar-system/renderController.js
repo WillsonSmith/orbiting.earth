3bb5b10332fac85f7477528fe71ff24222d7d98f
Willson Smith <me@willsonsmith.com>
2022-05-15 21:06:19 -0400

    can now add and remove dom nodes to update renderer

---
+++ shared/web-components/solar-system/renderController.js
@@ -0,0 +1,69 @@
+export class RenderController {
+  constructor(host, {engine, version, canvas}) {
+    this.engine = engine;
+    this.version = version;
+    this._bodies = new Map();
+    (this.host = host).addController(this);
+  }
+
+  hostConnected() {
+    console.log(this._renderConnectedLog(this.engine, this.version));
+  }
+  hostDisconnected() {
+    console.log(`Host • disconnected`);
+  }
+
+  addBody(body) {
+    this._bodies.set(body.name, body);
+    this.host.requestUpdate();
+  }
+
+  removeBody(name) {
+    this._bodies.delete(name);
+    this.host.requestUpdate();
+  }
+
+  updateBody(body) {
+    this._bodies.set(body.name, body);
+    this.host.requestUpdate();
+  }
+
+  renderBodies() {
+    if (this.engine === `canvas`) this.renderCanvas();
+  }
+
+  renderCanvas() {
+    requestAnimationFrame(() => {
+      if (!this.canvas) return;
+      const ctx = this.canvas.getContext(`2d`);
+      ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      for (const body of this._bodies.values()) {
+        let {name, position, radius, color} = body;
+        
+        if (body.orbits) {
+          const orbitedBody = this._bodies.get(body.orbits);
+
+          position = {
+            x: orbitedBody.position.x + 0.1,
+            y: orbitedBody.position.y + 0.05,
+          };
+        }
+        
+        const x = (position.x) * this.canvas.width / 2;
+        const y = (position.y) * this.canvas.height / 2;
+
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.closePath();
+      }
+    });
+  }
+
+  _renderConnectedLog (engine, version) {
+    return (`Renderrer • Connected`, {engine, version});
+  }
+}
--- shared/web-components/solar-system/solar-system.js
+++ shared/web-components/solar-system/solar-system.js
@@ -1,6 +1,7 @@
 import {LitElement, html, css} from 'lit';
 import './components/solar-system-body/solar-system-body.js';
 
+import {RenderController} from './renderController.js';
 class SolarSystem extends LitElement {
   static get properties() {
     return {
@@ -41,19 +42,32 @@ class SolarSystem extends LitElement {
     super.connectedCallback();
     this.addEventListener(`solar-system-body-added`, this.handleBodyAdded);
     this.addEventListener(`solar-system-body-changed`, this.handleBodyChanged);
-    this.addEventListener(`solar-system-body-removed`, this.handleBodyRemoved);
 
     const resizeObserver = new ResizeObserver(() => {
       this._renderCanvas();
     });
     resizeObserver.observe(this);
+
+    const mutationObserver = new MutationObserver((mutations) => {
+      const mutation = mutations[0];
+      if (mutation.removedNodes) {
+        for (const removedNode of mutation.removedNodes) {
+          if (removedNode.nodeName === `SOLAR-SYSTEM-BODY`) {
+            this.handleBodyRemoved(removedNode.name);
+          }
+        }
+      }
+    });
+    mutationObserver.observe(this, {
+      childList: true,
+      subtree: false
+    });
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
     this.removeEventListener(`solar-system-body-added`, this.handleBodyAdded);
     this.removeEventListener(`solar-system-body-changed`, this.handleBodyChanged);
-    this.removeEventListener(`solar-system-body-removed`, this.handleBodyRemoved);
   }
 
   render() {
@@ -64,8 +78,12 @@ class SolarSystem extends LitElement {
   }
 
   handleBodyAdded(event) {
-    // const {name, position, radius, color, texture} = event.detail;
     this.renderer.addBody(event.detail);
+    this._renderCanvas();
+  }
+
+  handleBodyRemoved(name) {
+    this.renderer.removeBody(name);
     this._renderCanvas();
   }
 
@@ -86,77 +104,7 @@ class SolarSystem extends LitElement {
     canvas.height = this.offsetHeight * window.devicePixelRatio;
     canvas.style.width = `${this.offsetWidth}px`;
     canvas.style.height = `${this.offsetHeight}px`;
-    canvas && this.renderer.renderBodies({canvas});
-  }
-}
-
-class RenderController {
-  constructor(host, {engine, version, canvas}) {
-    this.engine = engine;
-    this.version = version;
-    this._bodies = new Map();
-    (this.host = host).addController(this);
-  }
-
-  hostConnected() {
-    console.log(this._renderConnectedLog(this.engine, this.version));
-  }
-  hostDisconnected() {
-    console.log(`Host • disconnected`);
-  }
-
-  addBody(body) {
-    this._bodies.set(body.name, body);
-    this.host.requestUpdate();
-  }
-
-  removeBody(body) {
-    this._bodies.delete(body.name);
-    this.host.requestUpdate();
-  }
-
-  updateBody(body) {
-    this._bodies.set(body.name, body);
-    this.host.requestUpdate();
-  }
-
-  renderBodies() {
-    if (this.engine === `canvas`) this.renderCanvas();
-  }
-
-  renderCanvas() {
-    requestAnimationFrame(() => {
-      if (!this.canvas) return;
-      const ctx = this.canvas.getContext(`2d`);
-      ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-
-      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      for (const body of this._bodies.values()) {
-        let {name, position, radius, color} = body;
-        
-        if (body.orbits) {
-          const orbitedBody = this._bodies.get(body.orbits);
-
-          position = {
-            x: orbitedBody.position.x + 0.1,
-            y: orbitedBody.position.y + 0.05,
-          };
-        }
-        
-        const x = (position.x) * this.canvas.width / 2;
-        const y = (position.y) * this.canvas.height / 2;
-
-        ctx.beginPath();
-        ctx.arc(x, y, radius, 0, 2 * Math.PI);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.closePath();
-      }
-    });
-  }
-
-  _renderConnectedLog (engine, version) {
-    return (`Renderrer • Connected`, {engine, version});
+    canvas && this.renderer.renderBodies();
   }
 }
 
